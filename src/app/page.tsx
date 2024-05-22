91b9e55fab041a0fd08ec67318c37dc5ac21e921
pedrohmelo <phmc_1995@hotmail.com>
2024-05-21 23:12:07 -0300

    fon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
+import fon from "../../public/images/image.png";
 
 export default function Home() {
-  return <main className="">fon</main>;
+  return (
+    <main className="flex flex-col items-center justify-center pt-52">
+      <div className="font-bold text-8xl">Dream Team</div>
+      <Image src={fon} alt="fon" />
+    </main>
+  );
 }
